0a5d7a9950e2c7da1c656b752641e3e76fdeac2d
aarred7 <97483313+aarred7@users.noreply.github.com>
2022-05-03 22:28:24 -0500

    final_project

---
+++ questions.js
@@ -0,0 +1,70 @@
+const questions = [
+  {
+    "question": "Your interior decor is a reflection of who you are.",
+    "answer1": "Neutral",
+    "answer1Total": "1",
+    "answer2": "Disagree",
+    "answer2Total": "2",
+    "answer3": "Agree",
+    "answer3Total": "3"
+  },
+  {
+    "question": "I am very creative.",
+    "answer1": "Neutral",
+    "answer1Total": "1",
+    "answer2": "Disagree",
+    "answer2Total": "2",
+    "answer3": "Agree",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Select in which order you would value these \"Uniqueness, Design & Comfort",
+    "answer1": "Comfort, Design, Uniqueness",
+    "answer1Total": "1",
+    "answer2": "Design, Uniqueness, Comfort",
+    "answer2Total": "3",
+    "answer3": "Uniqueness, Design, Comfort",
+    "answer3Total": "2"
+  },
+  {
+    "question": "Best sentence to describe you.",
+    "answer1": "You like to take risks and try out new things.",
+    "answer1Total": "3",
+    "answer2": "You prefer comfort and simplicity over anything.",
+    "answer2Total": "2",
+    "answer3":
+      "You enjoy the sunlight and are mostly in a happy mood.",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Which best describes your style?",
+    "answer1": "Business Casual.",
+    "answer1Total": "1",
+    "answer2": "Minimalist.",
+    "answer2Total": "2",
+    "answer3": "Futuristic.",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Which do you prefer to have in your home?",
+    "answer1":
+      "Organic shaped furniture.",
+    "answer1Total": "3",
+    "answer2": "Geometric shaped furniture.",
+    "answer2Total": "2",
+    "answer3": "Wood decor.",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Which of the following color combinations do you like most?",
+    "answer1": "Orange and yellow",
+    "answer1Total": "1",
+    "answer2": "Black and white",
+    "answer2Total": "2",
+    "answer3": "Magenta and green",
+    "answer3Total": "3"
+  }
+]
+
